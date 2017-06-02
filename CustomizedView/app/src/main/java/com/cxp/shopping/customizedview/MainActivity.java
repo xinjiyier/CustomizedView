1ebaf17cfb41255f61ea1b70e58de9f2bccf2d6f
xinjiyier <874341753@qq.com>
2017-06-02 16:44:10 +0800

    自定义控件总结

---
+++ CustomizedView/app/src/main/java/com/cxp/shopping/customizedview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cxp.shopping.customizedview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+/**
+ * 这里主要是使用画布，画笔
+ * 将
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
